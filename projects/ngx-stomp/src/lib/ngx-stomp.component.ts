d23b1c00b43c8b4a2ba52313e87629889ce54cb0
kain <kainpro@foxmail.com>
2018-12-15 23:32:50 +0800

    Develop NGX-STOMP

--- projects/ngx-stomp/src/lib/ngx-stomp.component.ts
+++ projects/ngx-stomp/src/lib/ngx-stomp.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'lib-ngx-stomp',
@@ -11,7 +11,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NgxStompComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
