cbcf4b82e541f08e9f4c8c59e6277356c8ceae27
kain <kainpro@foxmail.com>
2018-12-15 23:25:15 +0800

    Add Library NGX-STOMP

---
+++ projects/ngx-stomp/src/lib/ngx-stomp.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-ngx-stomp',
+  template: `
+    <p>
+      ngx-stomp works!
+    </p>
+  `,
+  styles: []
+})
+export class NgxStompComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ projects/ngx-stomp/src/public_api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of ngx-stomp
+ */
+
+export * from './lib/ngx-stomp.service';
+export * from './lib/ngx-stomp.component';
+export * from './lib/ngx-stomp.module';
