3739b385203f109142a593c9f861d1626fc19778
kain <kainpro@foxmail.com>
2019-04-01 11:02:49 +0800

    submit

--- src/app/@message/mqtt.service.ts
+++ src/app/@message/mqtt.service.ts
@@ -1,7 +1,17 @@
-import {Injectable} from '@angular/core';
+import {Injectable, NgZone} from '@angular/core';
+import {AsyncSubject} from 'rxjs';
+import {MqttClient} from 'mqtt';
+
+declare let Client: MqttClient;
 
 @Injectable()
 export class MqttService {
-  constructor() {
+  private mqttClient: MqttClient;
+  ready: AsyncSubject<MqttClient> = new AsyncSubject();
+
+  client(brokerUrl: string, opts?: any) {
+    // this.mqttClient = connect(brokerUrl, opts);
+    // this.ready.next(this.mqttClient);
+    // this.ready.complete();
   }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,12 @@
 import {Component} from '@angular/core';
+import {MqttService} from './@message/mqtt.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
 export class AppComponent {
+  constructor(private mqttService: MqttService) {
+
+  }
 }
